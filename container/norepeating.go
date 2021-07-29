251ad5818c63c54aba47bfd69e3be45cf2761de2
bernylinville <bernylinville@devopsthink.org>
2021-07-29 17:01:48 +0800

    add rune

--- container/norepeating.go
+++ container/norepeating.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func lengthOfLongestSubstring(s string) int {
-	lastOccurred := make(map[byte]int)
+	lastOccurred := make(map[rune]int)
 	start := 0
 	maxlength := 0
-	for i, ch := range []byte(s) {
+	for i, ch := range []rune(s) {
 		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
 			start = lastI + 1
 		}
@@ -24,4 +24,7 @@ func main() {
 	fmt.Println(lengthOfLongestSubstring("bbbbb"))
 	fmt.Println(lengthOfLongestSubstring("pwwkew"))
 	fmt.Println(lengthOfLongestSubstring(""))
+	fmt.Println(lengthOfLongestSubstring("这是一个测这试的桑出去个的"))
+	fmt.Println(lengthOfLongestSubstring("为什噩梦为晒为喵额为什么塞饭传闻塞饭吃为什喵额"))
+	fmt.Println(lengthOfLongestSubstring("一二三三二一"))
 }
