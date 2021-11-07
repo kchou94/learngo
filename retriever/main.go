4f1a15fded8b4df6b2d925cc766f6da9219a39f5
Kyle Chou <kylechou@devopsthink.org>
2021-11-07 11:05:43 +0800

    update

--- retriever/main.go
+++ retriever/main.go
@@ -11,19 +11,68 @@ type Retriever interface {
 	Get(url string) string
 }
 
+type Poster interface {
+	Post(url string, form map[string]string) string
+}
+
+type RetrieverPoster interface {
+	Retriever
+	Poster
+}
+
+const url = "http://www.baidu.com"
+
 func download(r Retriever, url string) string {
 	return r.Get(url)
 }
 
-func main() {
-	var r Retriever
-	r = mock.Retriever{"this is a fake news"}
+func inspect(r Retriever) {
 	fmt.Printf("%T %v\n", r, r)
 
-	r = real.Retriever{
+	switch v := r.(type) {
+	case *mock.Retriever:
+		fmt.Println("Contents:", v.Contents)
+	case *real.Retriever:
+		fmt.Println("UserAgent:", v.UserAgent)
+	}
+}
+
+func Post(poster Poster) {
+	poster.Post(url, map[string]string{
+		"name":   "ccmouse",
+		"course": "golang",
+	})
+}
+
+func session(s RetrieverPoster) string {
+	s.Post(url, map[string]string{
+		"contents": "another faked news",
+	})
+	return s.Get(url)
+}
+
+func main() {
+	var r Retriever
+	retriever := mock.Retriever{"this is a fake news"}
+	r = &retriever
+	inspect(r)
+
+	r = &real.Retriever{
 		UserAgent: "Mozilla/5.0",
 		TimeOut:   time.Minute,
 	}
-	fmt.Printf("%T %v\n", r, r)
+
+	inspect(r)
+
+	// Type assertion
+	if realRetriever, ok := r.(*mock.Retriever); ok {
+		fmt.Println(realRetriever.Contents)
+	} else {
+		fmt.Println("not a mock retriever")
+	}
+
 	// fmt.Println(download(r, "http://www.baidu.com"))
+
+	fmt.Println("Try a session")
+	fmt.Println(session(&retriever))
 }
