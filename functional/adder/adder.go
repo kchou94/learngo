37345abf19bdb7172bc6eafb9693d39171229af3
berny linville <bernylinville@devopsthink.org>
2021-11-07 17:23:25 +0800

    update

--- functional/adder/adder.go
+++ functional/adder/adder.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func adder() func(value int) int {
 	sum := 0
 
@@ -9,9 +11,26 @@ func adder() func(value int) int {
 	}
 }
 
+type iAdder func(value int) (int, iAdder)
+
+func adder2(base int) iAdder {
+	return func(value int) (int, iAdder) {
+		return base + value, adder2(base + value)
+	}
+}
+
 func main() {
 	adder := adder()
 	for i := 0; i < 10; i++ {
 		println(adder(i))
+	}
+
+	fmt.Println()
+
+	a := adder2(0)
+	for i := 0; i < 10; i++ {
+		var s int
+		s, a = a(i)
+		fmt.Println(s)
 	}
 }
